0b9fc4e7ac01bd28dafde8933755cebea5777caf
Amit Panwar <21213888+amitpanwarIndia@users.noreply.github.com>
2023-10-23 00:07:42 +0530

    Update run_classifier.py
    changed flag lib

--- run_classifier.py
+++ run_classifier.py
@@ -25,8 +25,10 @@ import modeling
 import optimization
 import tokenization
 import tensorflow as tf
+from absl import app
+from absl import flags
 
-flags = tf.flags
+#flags = tf.flags
 
 FLAGS = flags.FLAGS
 
@@ -99,25 +101,25 @@ flags.DEFINE_integer("iterations_per_loop", 1000,
 
 flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
 
-tf.flags.DEFINE_string(
+flags.DEFINE_string(
     "tpu_name", None,
     "The Cloud TPU to use for training. This should be either the name "
     "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
     "url.")
 
-tf.flags.DEFINE_string(
+flags.DEFINE_string(
     "tpu_zone", None,
     "[Optional] GCE zone where the Cloud TPU is located in. If not "
     "specified, we will attempt to automatically detect the GCE project from "
     "metadata.")
 
-tf.flags.DEFINE_string(
+flags.DEFINE_string(
     "gcp_project", None,
     "[Optional] Project name for the Cloud TPU-enabled project. If not "
     "specified, we will attempt to automatically detect the GCE project from "
     "metadata.")
 
-tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
+flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
 
 flags.DEFINE_integer(
     "num_tpu_cores", 8,
